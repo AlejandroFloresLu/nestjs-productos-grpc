49f1e40ff75c2bf998a4804ef91f8c04302576f3
semestre5grupla <flores.alejandro10moc@gmail.com>
2026-09-23 10:59:47 -0500

    fix: bind to process.env.PORT for Render support

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,10 @@ async function bootstrap() {
     options: {
       package: 'productos',
       protoPath: join(__dirname, 'productos.proto'),
-      url: '0.0.0.0:5000',
+      url: `0.0.0.0:${process.env.PORT || 5000}`,
     },
   });
   await app.listen();
-  console.log('Microservicio gRPC escuchando en 0.0.0.0:5000');
+  console.log(`Microservicio gRPC escuchando en 0.0.0.0:${process.env.PORT || 5000}`);
 }
 bootstrap();
